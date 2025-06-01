494f353af74f9fb88d0d1f77bcfd2998ccdffadd
LieQuz <liequz@gmail.com>
2025-06-01 18:03:37 +0900

    スクロールできないように変更

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
       sizeWidth += (13 - sizeWidth) * 0.05;
       sizeHeight += (13 - sizeHeight) * 0.05;
-      
+
       cursor.style.backgroundColor = 'rgb(255, 255, 255)';
       cursor.style.width = sizeWidth + 'px';
       cursor.style.height = sizeHeight + 'px';
